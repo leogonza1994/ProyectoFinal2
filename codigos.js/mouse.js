3801bc61ca1803e79dd79cbcd62ea73aabca91d4
leogonza1994 <dlgc12@hotmail.com>
2020-12-03 19:37:22 -0500

    Repositorio Unidad 2

---
+++ codigos.js/mouse.js
@@ -0,0 +1,9 @@
+$(document).mousemove(function(event){
+
+    $("#alien").css({
+
+    left: event.pageX,
+    top: event.pageY    
+    })
+
+});
---
+++ codigos.js/move.js
@@ -0,0 +1,7 @@
+$(".item1").click(function(){
+    var div = $(".item1");
+    div.animate({height: '300px', opacity: '0.4'}, "slow");
+    div.animate({width: '300px', opacity: '0.8'}, "slow");
+    div.animate({height: '100px', opacity: '0.4'}, "slow");
+    div.animate({width: '100px', opacity: '0.8'}, "slow");
+  }); 
